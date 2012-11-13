8301c18868e0ff55ef4ef2ba63abdf2f2c571d7f
robindecroon <robindecroon@msn.com>
2012-11-13 14:00:38 +0100

    statistica zonder animatie, zonder scrollview

--- iOS/Felicity/Felicity/OutputViewController.m
+++ iOS/Felicity/Felicity/OutputViewController.m
@@ -37,84 +37,45 @@
 */
 - (void)createStatistics {
     
-    NSArray *emotions= [[Database database] retrieveEmotionsFromDatabase];
+    for(UIView *subview in [self.view subviews]) {
+        [subview removeFromSuperview];
+    }
     
+    NSArray *sortedStatistics = [FelicityUtil retrieveEmotionStatistics];
+    
+    double maxPercentage = ((EmotionStatistics *)sortedStatistics[0]).percentageSelected;
+        
     int xPadding = 10;
     int yPadding = 10;
     int imageSize = 50;
     int yMargin = 70;
-    NSArray *sortedStatistics = [FelicityUtil retrieveEmotionStatistics];
+    int xWidthBar = 230;
     
-    for(int i = 0; i < emotions.count; i++) {
+    for(int i = 0; i < sortedStatistics.count; i++) {
+        // Nodige dynamische gegevens
         EmotionStatistics *statistics = (EmotionStatistics *)sortedStatistics[i];
         Emotion *emotion = statistics.emotion;
+        double percentage = statistics.percentageSelected;
         
+        // De afbeeldingen
         CGRect Imageframe;
         Imageframe.origin.x = xPadding;
         Imageframe.origin.y = yPadding + yMargin*(i);
         Imageframe.size = CGSizeMake(imageSize,imageSize);
+        UIImageView *imageSubView = [[UIImageView alloc] initWithFrame:Imageframe];
+        imageSubView.image = [UIImage imageNamed:emotion.smallImage];
+        [self.view addSubview:imageSubView];
         
-        UIImageView *subview = [[UIImageView alloc] initWithFrame:Imageframe];
-        subview.image = [UIImage imageNamed:emotion.smallImage];
+        // De bars
+        UILabel *barSubView = [[UILabel alloc] initWithFrame:CGRectMake(xPadding + 60, yPadding + yMargin*(i), (xWidthBar / maxPercentage) * percentage, imageSize)];
+        barSubView.text = [[NSString alloc] initWithFormat: @"%.2f %%", percentage*100];
+        barSubView.textAlignment = NSTextAlignmentRight;
+        barSubView.textColor = [UIColor whiteColor];
+        barSubView.backgroundColor = [UIColor darkGrayColor];
+        [barSubView setFont:[UIFont fontWithName:@"Arial" size:11]];
+        [self.view addSubview:barSubView];
         
-        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(xPadding + 60, yPadding + yMargin*(i), 230, imageSize)];
-        label.text = [[NSString alloc] initWithFormat: @"%f", statistics.percentageSelected];
-        label.textAlignment = NSTextAlignmentRight;
-        label.textColor = [UIColor whiteColor];
-        label.backgroundColor = [UIColor blackColor];
-        [label setFont:[UIFont fontWithName:@"Arial" size:12]];
-        [self.view addSubview:label];
-        
-        [self.view addSubview:subview];
-        
-        /*Emotion *emotion = emotions[i];
-        
-        EmotionStatistics *statistics = [[Database database] retrieveEmotionStaticsForEmotion:emotion];
-        
-        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(60, 20 + 22*i, 110, 20)];
-        label.text = [emotion.displayName stringByAppendingString:@"; "];
-        label.textAlignment = NSTextAlignmentRight;
-        label.textColor = [UIColor whiteColor];
-        label.backgroundColor = [UIColor blackColor];
-        label.numberOfLines = 1;
-        [label setFont:[UIFont fontWithName:@"Arial" size:14]];
-        [self.view addSubview:label];
-        
-        UILabel *timesSelected = [[UILabel alloc] initWithFrame:CGRectMake(190, 20 + 22*i, 200, 20)];
-        timesSelected.text = [[NSString alloc] initWithFormat: @"%f", statistics.percentageSelected];
-        timesSelected.textAlignment = NSTextAlignmentLeft;
-        timesSelected.textColor = [UIColor whiteColor];
-        timesSelected.backgroundColor = [UIColor blackColor];
-        timesSelected.numberOfLines = 1;
-        [timesSelected setFont:[UIFont fontWithName:@"Arial" size:14]];
-        [self.view addSubview:timesSelected];*/
     }
-    
-    
-    
-    /*
-     
-     NSString *imageSourceName = [[emotions objectAtIndex:i] smallImage];
-     UIImage *image = [UIImage imageNamed:imageSourceName];
-     
-     CGRect frame;
-     frame.origin.x = 8 + 80*(i % 4);
-     frame.origin.y = 10 + 87*(i / 4);
-     frame.size = CGSizeMake(64,64);
-     
-     UILabel *imageName = [[UILabel alloc] initWithFrame:CGRectMake(80*(i % 4), 75 + 87*(i / 4), 80, 15)];
-     imageName.text = [emotions[i] displayName];
-     imageName.textAlignment = NSTextAlignmentCenter;
-     imageName.textColor = [UIColor whiteColor];
-     imageName.backgroundColor = [UIColor blackColor];
-     [imageName setFont:[UIFont fontWithName:@"Arial" size:12]];
-     [self.emotionsOverviewView addSubview:imageName];
-     
-     UIImageView *subview = [[UIImageView alloc] initWithFrame:frame];
-     subview.image = image;
-     
-     [self.emotionsOverviewView addSubview:subview];
-    */
 }
 
 /*
