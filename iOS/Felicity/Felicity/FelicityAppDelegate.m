c1aaebaac6881097ac4d08756bf506a54ac7f77a
robindecroon <robindecroon@msn.com>
2012-11-13 11:06:04 +0100

    printCurrentHistory moet niet meer uitgevoerd worden

--- iOS/Felicity/Felicity/FelicityAppDelegate.m
+++ iOS/Felicity/Felicity/FelicityAppDelegate.m
@@ -35,9 +35,7 @@
     if ([CLLocationManager locationServicesEnabled]== NO) {
         UIAlertView *servicesDisabledAlert = [[UIAlertView alloc] initWithTitle:@"Location Services Disabled" message:@"You currently have all location services for this device disabled. If you proceed, you will be asked to confirm whether location services should be reenabled." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
         [servicesDisabledAlert show];
-    }
-    [[Database database] printCurrentHistory];
-            
+    }            
     return YES;
 }
 							
