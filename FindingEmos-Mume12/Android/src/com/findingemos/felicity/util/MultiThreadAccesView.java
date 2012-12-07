af57640ee8d545b38dffeb54f930ed459812a962
robindecroon <robin.decroon@student.kuleuven.be>
2012-12-07 20:59:31 +0100

    Dubbele map Finding emos verwijderd

--- FindingEmos-Mume12/Android/src/com/findingemos/felicity/util/MultiThreadAccesView.java
+++
@@ -1,8 +0,0 @@
-package com.findingemos.felicity.util;
-
-public interface MultiThreadAccesView {
-	/**
-	 * Requests a checked update for the drawer.
-	 */
-	public void requestSyncUpdate();
-}
--- FindingEmos-Mume12/HTML5/Readme.txt
+++
@@ -1,1 +0,0 @@
-HTML code should go here
