6f7f3b428c87bb87fa038c4f60632c1f42ee8d01
robindecroon <robin.decroon@student.kuleuven.be>
2012-12-22 09:27:14 -0800

    Constants files without tokens

---
+++ Android/src/com/findingemos/felicity/twitter/Constants.java
@@ -0,0 +1,18 @@
+package com.findingemos.felicity.twitter;
+
+public class Constants {
+
+  public static final String CONSUMER_KEY = "makeyourown ;-)";
+	public static final String CONSUMER_SECRET= "makeyourown ;-)";
+	
+	public static final String REQUEST_URL = "https://api.twitter.com/oauth/request_token";
+	public static final String ACCESS_URL = "https://api.twitter.com/oauth/access_token";
+	public static final String AUTHORIZE_URL = "https://api.twitter.com/oauth/authorize";
+	
+	public static final String	OAUTH_CALLBACK_SCHEME	= "x-oauthflow-twitter";
+	public static final String	OAUTH_CALLBACK_HOST		= "callback";
+	public static final String	OAUTH_CALLBACK_URL		= OAUTH_CALLBACK_SCHEME + "://" + OAUTH_CALLBACK_HOST;
+	
+	public static final int TWEET_LIMIT = 140;
+
+}
